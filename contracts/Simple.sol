03809acccb242ca1a84562532d7c8484c2310a22
peng.chen <chen_peng7@163.com>
2023-04-08 19:27:00 +0800

    20230408 add simple contract

--- contracts/Simple.sol
+++ contracts/Simple.sol
@@ -2,10 +2,37 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Simple {
-  mapping(address => uint) public balance
+    address public owner;
+    mapping(address => uint256) public balance;
 
-  constructor() public {
-  }
+    constructor() {
+        owner = msg.sender;
+    }
 
-  modifier 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
+    }
+
+    function airdrop(address receiver, uint256 number)
+        public
+        payable
+        onlyOwner
+        returns (uint256)
+    {
+        balance[receiver] += number;
+        return balance[receiver];
+    }
+
+    function getBalance(address addr) public view returns (uint256) {
+        return balance[addr];
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    function sayHello() public pure returns (string memory) {
+        return "hello wold!";
+    }
 }
